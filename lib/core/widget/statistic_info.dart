d0c83a36a9316d2652a2e041b91d6fac3289695f
Cihangir Tuncer <cihangiraa@cntbilisim.com.tr>
2021-11-14 23:05:29 +0300

    push

--- lib/core/widget/statistic_info.dart
+++ lib/core/widget/statistic_info.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 statisticInfo(
     {List<Widget> children, Widget subtitle, Widget leading, Widget trailing}) {
   return Column(
-      crossAxisAlignment: CrossAxisAlignment.start, children: children);
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: children,
+  );
 }
