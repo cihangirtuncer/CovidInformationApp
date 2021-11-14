86683c37fcf0e729a821a3396d84eff5588a6f41
Cihangir Tuncer <cihangiraa@cntbilisim.com.tr>
2021-11-15 00:17:05 +0300

    push

--- lib/core/components/app_bar/main_app_bar.dart
+++ lib/core/components/app_bar/main_app_bar.dart
@@ -4,6 +4,9 @@ AppBar mainAppBar(String title, Color color) {
   return AppBar(
     centerTitle: true,
     backgroundColor: color,
-    title: Text(title),
+    title: Text(
+      title,
+      style: const TextStyle(color: Colors.amber, fontSize: 25),
+    ),
   );
 }
